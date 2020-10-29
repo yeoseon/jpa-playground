883ea72235a8e44c28f8364d403658603340e715
Yeoseon Yun <devyyskr@gmail.com>
2020-10-29 10:16:41 +0900

    refactor(JpaMain): 정석 코드로 작성. 오류가 발생할 경우 Rollback 처리 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,17 +16,21 @@ public class JpaMain {
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        Member member = new Member();
+        try {
+            Member member = new Member();
 
-        member.setId(1L);
-        member.setName("HelloA");
+            member.setId(1L);
+            member.setName("HelloA");
 
-        em.persist(member);
-
-        tx.commit();
+            em.persist(member);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();     // 꼭 닫아줘야 한다.
+        }
 
         // 사용 후 둘다 Close 해줘야 한다.
-        em.close();
         emf.close();
     }
 }
