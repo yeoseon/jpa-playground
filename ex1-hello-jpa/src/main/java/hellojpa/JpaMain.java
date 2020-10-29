c7af5b798e3526e57451e062ecd1b568f39246eb
Yeoseon Yun <devyyskr@gmail.com>
2020-10-29 10:14:29 +0900

    feat(JpaMain): ENtity 추가 및 데이터 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,17 +2,31 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import javax.swing.*;
 
 public class JpaMain {
 
     public static void main(String[] args) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");     // emf를 만드는 순간, DB와 연결도 되고 각종 설정도 다된다.
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");     // emf를 만드는 순간, DB와 연결도 되고 각종 설정도 다된다. **로딩 시점에 딱 하나만 만들어야 한다.**
+        EntityManager em = emf.createEntityManager();    // EntityManager 를 꺼내서 사용을 위한 Code를 작성한다. Query를 수행할 때마다 하나씩 만들어졌다가 close 되어야 한다.
 
-        EntityManager entityManager = emf.createEntityManager();    // EntityManager 를 꺼내서 사용을 위한 Code를 작성한다.
+        // JPA는 Transaction 설정이 가장 중요하므로 아래와 같이 Transaction을 얻어서 시작처리 해야한다.
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        Member member = new Member();
+
+        member.setId(1L);
+        member.setName("HelloA");
+
+        em.persist(member);
+
+        tx.commit();
 
         // 사용 후 둘다 Close 해줘야 한다.
-        entityManager.close();
+        em.close();
         emf.close();
     }
 }
