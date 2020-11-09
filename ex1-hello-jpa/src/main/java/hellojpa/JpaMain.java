6c4bbf8b9d40fd32e911025061411ce898a1e953
Yeoseon Yun <devyyskr@gmail.com>
2020-11-09 10:13:36 +0900

    필드와 컬럼 맵핑 예시 추가

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -16,33 +16,33 @@ public class JpaMain {
         tx.begin();
 
         try {
-            // 비영속 상태
-            Member member = new Member();
-            member.setId(101L);
-            member.setName("HelloJPA");
-
-            // 영속 상태
-            // em안의 영속성 컨텍스트 내부에 들어가 관리된다.
-            // 사실은 이때 DB에 저장되는 것이 아니다.
-            System.out.println("------------- BEFORE --------");
-            em.persist(member); // 사실 여기에서는 Query가 나가지 않는다.
-            System.out.println("------------- AFTER --------");
-
-            // 이 또한 영속 상태
-            Member findMember = em.find(Member.class, 101L);
-            member.setName("AAAAA");  // 변경 감지가 되어 Update SQL이 쓰기 지연 SQL 저장소에 저장된다.
-
-            // 준영속 상태로 영속상태에서 벗어나게 했다.
-            // 영속상태로 더이상 관리하지 말라는 의미이므로, 이 관련된 모든게 영속성 컨텍스트에서 빠진다.
-            // 따라서 위에서 Setter를 썼더라도, 해당 SQL은 삭제되어 commit 시점에 update 처리되지 않는다.
-            em.detach(member);
-
-            // 조회용 SQL이 수행되지 않는다.
-            // 1차 캐시에서 조회해오기 때문
-            System.out.println(findMember.getId());
-            System.out.println(findMember.getName());
-
-            // 사실은 이 시점에 쿼리가 날라가는 것이다.
+//            // 비영속 상태
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("HelloJPA");
+//
+//            // 영속 상태
+//            // em안의 영속성 컨텍스트 내부에 들어가 관리된다.
+//            // 사실은 이때 DB에 저장되는 것이 아니다.
+//            System.out.println("------------- BEFORE --------");
+//            em.persist(member); // 사실 여기에서는 Query가 나가지 않는다.
+//            System.out.println("------------- AFTER --------");
+//
+//            // 이 또한 영속 상태
+//            Member findMember = em.find(Member.class, 101L);
+//            member.setName("AAAAA");  // 변경 감지가 되어 Update SQL이 쓰기 지연 SQL 저장소에 저장된다.
+//
+//            // 준영속 상태로 영속상태에서 벗어나게 했다.
+//            // 영속상태로 더이상 관리하지 말라는 의미이므로, 이 관련된 모든게 영속성 컨텍스트에서 빠진다.
+//            // 따라서 위에서 Setter를 썼더라도, 해당 SQL은 삭제되어 commit 시점에 update 처리되지 않는다.
+//            em.detach(member);
+//
+//            // 조회용 SQL이 수행되지 않는다.
+//            // 1차 캐시에서 조회해오기 때문
+//            System.out.println(findMember.getId());
+//            System.out.println(findMember.getName());
+//
+//            // 사실은 이 시점에 쿼리가 날라가는 것이다.
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -1,31 +1,24 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 public class Member {
 
     @Id
     private Long id;
+    @Column(name = "name")
+    private String username;
+    private Integer age;
+    @Enumerated(EnumType.STRING)        // DB에는 없는 Enum 타입 사용을 위해 해당 어노테이션을 붙힌다.
+    private RoleType roleType;
+    @Temporal(TemporalType.TIMESTAMP)   // Date, Time, TIMESTAMP
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+    @Lob                                // DLOB, CLOB 사용을 위해 설정
+    private String description;
 
-    @Column(unique = true, length = 10)
-    private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
+    public Member() {}
 }
