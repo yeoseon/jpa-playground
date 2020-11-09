dd4cc39ed56ca7385792e6ce79e50d8252a043e3
Yeoseon Yun <devyyskr@gmail.com>
2020-11-09 10:26:46 +0900

    @Column 어노테이션의 다양한 속성 예시

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -8,7 +8,7 @@ public class Member {
 
     @Id
     private Long id;
-    @Column(name = "name")
+    @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
     private String username;
     private Integer age;
     @Enumerated(EnumType.STRING)        // DB에는 없는 Enum 타입 사용을 위해 해당 어노테이션을 붙힌다.
@@ -17,7 +17,7 @@ public class Member {
     private Date createdDate;
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastModifiedDate;
-    @Lob                                // DLOB, CLOB 사용을 위해 설정
+    @Lob                                // DLOB, CLOB 사용을 위해 설정 (@Lob에 String 타입이면 CLOB으로 사용된다.)
     private String description;
 
     public Member() {}
