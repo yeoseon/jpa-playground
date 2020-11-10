990b6e43f7a2aabc0a4dd0f6cd9877fedcad4e1e
Yeoseon Yun <devyyskr@gmail.com>
2020-11-10 10:14:15 +0900

    시퀀스를 테이블마다 따로 관리하고 싶을 때, SEQUENCE generator 사용 예시

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,10 +4,15 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
+@SequenceGenerator(
+        name = "MEMBER_SEQ_GENERATOR",
+        sequenceName = "MEMBER_SEQ", // 맵핑할 데이터베이스 시퀀스 이름
+        initialValue =  1, allocationSize = 1)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+                generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
     private String username;
