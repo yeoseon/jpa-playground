d0eba16310ab91ab9ee28ad44c77215952233da3
Yeoseon Yun <devyyskr@gmail.com>
2020-11-10 11:01:59 +0900

    @Table 전략 예시 추가

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -4,14 +4,18 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@SequenceGenerator(
+//@SequenceGenerator(
+//        name = "MEMBER_SEQ_GENERATOR",
+//        sequenceName = "MEMBER_SEQ", // 맵핑할 데이터베이스 시퀀스 이름
+//        initialValue =  1, allocationSize = 1)
+@TableGenerator(
         name = "MEMBER_SEQ_GENERATOR",
-        sequenceName = "MEMBER_SEQ", // 맵핑할 데이터베이스 시퀀스 이름
-        initialValue =  1, allocationSize = 1)
+        table = "MY_SEQUENCES",
+        pkColumnValue = "MEMBER_SEQ", allocationSize = 1)
 public class Member {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE,
+    @GeneratedValue(strategy = GenerationType.TABLE,
                 generator = "MEMBER_SEQ_GENERATOR")
     private Long id;
     @Column(name = "name", nullable = false, columnDefinition = "varchar(100) default 'EMPTY'")
